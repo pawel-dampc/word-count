15736f60085db6b8b0a1d508523ac99790232a47
Pawel <dampc86pawel@gmail.com>
2021-08-31 22:29:57 +0200

    Initial commit

---
+++ src/WordCount.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String [] args) throws Exception {
+        File file = new File("book.txt");
+        Scanner scanner = new Scanner(file);
+
+        int words = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            words += line.split("").length;
+
+        }
+        System.out.println("The file contains: " + words + " words.");
+    }
+}
